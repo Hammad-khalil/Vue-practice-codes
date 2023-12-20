771af4ca9068d70a42f31e4664622b95520a4eda
Hammad-khalil <shhamadkhalil0786@gmail.com>
2023-12-20 23:44:05 +0500

    Simple Routing Done Next Step Dynamic RoutinG

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,17 @@
 <template>
   <router-view></router-view>
-  <router-link to="/src/components/Home-comp.vue" />
+  <router-link class="links" to="/">Home</router-link>
+  <router-link class="links" to="/about">About</router-link>
 </template>
 
 <script>
 export default {};
 </script>
+
+<style>
+.links {
+  margin: 20px;
+  position: relative;
+  bottom: 232px;
+}
+</style>
