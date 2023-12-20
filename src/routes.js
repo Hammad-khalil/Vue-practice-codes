f05397f546b01481c6f42cffe450b6837e78a8ec
Hammad-khalil <shhamadkhalil0786@gmail.com>
2023-12-20 21:37:17 +0500

    Using Router and created routes.js file successfully

---
+++ src/routes.js
@@ -0,0 +1,23 @@
+import { createRouter, createWebHistory } from "vue-router";
+import Home from "./components/Home-comp.vue"
+import About from "./components/About-comp.vue"
+
+const routes = [
+    {
+        name: 'Home',
+        path: '/',
+        component: 'Home'
+    },
+    {
+        name: 'About',
+        path: '/about',
+        component: 'About'
+    }
+]
+
+const router = createRouter({
+    history: createWebHistory,
+    routes
+});
+
+export default router;
